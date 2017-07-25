d38e9bf2ed12b7e44c0486751b72a46765f45e38
Vincent Legoll <vincent.legoll@gmail.com>
2017-07-25 18:40:03 +0200

    Pass ARCH & DISTID variables to enable raspbian/armhf builds

--- openio-debbuild/doit.sh
+++ openio-debbuild/doit.sh
@@ -3,4 +3,4 @@
 cd "deb-packaging"
 git pull origin master
 cd "${OIO_DISTRO}-${OIO_DISTRO_VER}/${OIO_PACKAGE}"
-bash ../../oio-debbuild.sh "${UPLOAD_RESULT}"
+DISTID=${OIO_DISTRO} ARCH=${OIO_ARCH} bash ../../oio-debbuild.sh "${UPLOAD_RESULT}"
