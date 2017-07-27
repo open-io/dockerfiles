f229ffb83253a9aa28c6c3d35ca121d613d0e1ec
Vincent Legoll <vincent.legoll@gmail.com>
2017-07-27 12:14:19 +0200

    Ensure we can run arm binaries automagically through qemu/binfmt

--- openio-debbuild/doit.sh
+++ openio-debbuild/doit.sh
@@ -3,4 +3,11 @@
 cd "deb-packaging"
 git pull origin master
 cd "${OIO_DISTRO}-${OIO_DISTRO_VER}/${OIO_PACKAGE}"
+
+# Ensure we can run arm binaries automagically through qemu via binfmt_misc
+update-binfmts --display | grep -q 'qemu-arm (enabled)'
+if [ $? -ne 0 ]; then
+    sudo update-binfmts --enable qemu-arm
+fi
+
 DISTID=${OIO_DISTRO} ARCH=${OIO_ARCH} bash ../../oio-debbuild.sh "${UPLOAD_RESULT}"
