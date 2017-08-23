137c727372c7eb5aa0a66d1c53b57c36c14996a1
Vincent Legoll <vincent.legoll@gmail.com>
2017-08-23 17:24:08 +0200

    Reactivate gpgcheck in mock config for prod
    Now the official key has to be used to build packages

--- openio-rpmbuild/build.py
+++ openio-rpmbuild/build.py
@@ -294,11 +294,6 @@ def patch_mock_config(distribution):
         repodata = get_repo_data()
         repodata.update({'repo_ip': repo_ip, 'repo_port': '8080'})
         newlines.append('baseurl=http://%(repo_ip)s:%(repo_port)s/pub/repo/%(company)s/%(prod)s/%(prod_ver)s/%(distro)s/%(distro_ver)s/%(arch)s/\n' % repodata)
-      elif line.startswith('gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-OPENIO-0'):
-        newlines.append('gpgkey=http://%(repo_ip)s:%(repo_port)s/pub/keyFile.pub\n' % {'repo_ip': repo_ip, 'repo_port': '8080'})
-        inopeniosection = True
-      elif line.startswith('gpgcheck=1') and inopeniosection:
-        newlines.append('gpgcheck=0\n')
       else:
         newlines.append(line)
   if not os.path.exists('/home/builder/.config'):
