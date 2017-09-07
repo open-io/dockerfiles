6351133d6b2b0b069f8c22b7b7396bfa65b38711
Vincent Legoll <vincent.legoll@gmail.com>
2017-09-07 14:55:02 +0200

    Delete obsolete image (now swift is also available from openio/sds images)

--- openio-sds-ext-swift/docker-ext-swift-keystone.pp
+++
@@ -1,9 +0,0 @@
-### Install and configure Keystone
-class { 'keystone':
-  verbose             => True,
-  admin_token         => 'KEYSTONE_ADMIN_UUID',
-  database_connection => 'sqlite:////var/lib/keystone/keystone.db',
-  manage_service      => false,
-  paste_config        => '/etc/keystone/keystone-paste.ini',
-  enable_bootstrap    => True,
-}
