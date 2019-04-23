7169a39b2a612431d76c8689d498acf33607d98f
Delgehier Cédric <cedric.delgehier@gmail.com>
2019-04-23 19:33:50 +0000

    FEATURE(sds): build of 19.04
     ##### SUMMARY
    
     ##### IMPACT
    N/A
    
     ##### ADDITIONAL INFORMATION

---
+++ openio-sds/19.04/centos/7/build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -ex
+
+OIOSDS_RELEASE=19.04
+DESTINATION=openio/sds:${OIOSDS_RELEASE}
+
+ID=$(docker run --detach --privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro --hostname openiosds centos/systemd:latest /usr/sbin/init)
+
+virtualenv venv
+. ./venv/bin/activate
+pip install 'ansible<2.8' netaddr
+git clone -b ${OIOSDS_RELEASE} https://github.com/open-io/ansible-playbook-openio-deployment.git
+cp inventory.yml ansible-playbook-openio-deployment/products/sds
+pushd ansible-playbook-openio-deployment/products/sds
+
+#custom inventory
+sed -i -e "s@ansible_host: ID@ansible_host: $ID@" inventory.yml
+
+
+# Download roles
+./requirements_install.sh
+
+# Deploy without bootstrap
+ansible-playbook -i inventory.yml main.yml --skip-tags checks
+
+if [ $? -eq 0 ]; then
+  # Fix redis: remove cluster mode
+  ansible openio -i inventory.yml -m shell -a 'sed -i -e "/slaveof/d" /etc/oio/sds/OPENIO/redis-0/redis.conf; rm /etc/gridinit.d/OPENIO-redissentinel-0.conf'
+  # Wipe install logs
+  ansible openio -i inventory.yml -m shell -a "find /var/log/oio -type f | xargs -n1 cp /dev/null"
+
+  popd
+  # Logs to stdout
+  ansible node1 -i ansible-playbook-openio-deployment/products/sds/inventory.yml -m copy -a 'src=rsyslog.conf dest=/etc/rsyslog.d/openio-sds.conf mode=0644'
+
+  # Copy entrypoint
+  ansible node1 -i ansible-playbook-openio-deployment/products/sds/inventory.yml -m copy -a 'src=openio-docker-init.sh dest=/openio-docker-init.sh mode=0755'
+
+  docker commit --change='CMD ["/openio-docker-init.sh"]' --change "EXPOSE 6000 6001 6006 6007 6009 6011 6014 6017 6110 6120 6200 6300" ${ID} ${DESTINATION}
+  docker stop ${ID}
+  docker rm ${ID}
+fi
---
+++ openio-sds/19.04/centos/7/openio-docker-init.sh
@@ -0,0 +1,218 @@
+#!/bin/sh
+
+# Default configuration
+NS='OPENIO'
+NBREPLICAS=1
+TIMEOUT=20
+
+function prepare(){
+  mkdir /run/gridinit
+  install -d -o openio -g openio -m 750 /run/oio/sds/OPENIO/
+}
+
+function update_swift_credentials(){
+  if [ ! -z "${SWIFT_CREDENTIALS}" ]; then
+    # Remove default credentials
+    sed -i -e '/user_demo_demo = DEMO_PASS .member/d' \
+      /etc/oio/sds/OPENIO/oioswift-0/proxy-server.conf
+    # Add credentials to the Swift proxy configuration
+    IFS=',' read -r -a swiftcreds <<< "${SWIFT_CREDENTIALS}"
+    for creds in "${swiftcreds[@]}"
+    do
+      echo "> Adding Openstack Swift credentials $creds"
+      IFS=':' read -r -a cred <<< "$creds"
+      sed -i -e "s@^use = egg:oioswift#tempauth\$@use = egg:oioswift#tempauth\nuser_${cred[0]}_${cred[1]} = ${cred[2]} ${cred[3]}@" \
+        /etc/oio/sds/OPENIO/oioswift-0/proxy-server.conf
+      if [ "${creds}" == "${swiftcreds[0]}" ]; then
+        sed -i -e "s@aws_access_key_id = demo:demo@aws_access_key_id = ${cred[0]}:${cred[1]}@" /root/.aws/credentials
+        sed -i -e "s@aws_secret_access_key = DEMO_PASS@aws_secret_access_key = ${cred[2]}@" /root/.aws/credentials
+        AKEY="${cred[0]}:${cred[1]}"
+				SKEY="${cred[2]}"
+      fi
+    done
+  else
+    AKEY="demo:demo"
+    SKEY="DEMO_PASS"
+  fi
+}
+
+function set_region(){
+  if [ ! -z "${REGION}" ]; then
+    echo "> Change S3 region"
+    sed -i -e "s@location = us-east-1@location = ${REGION}@" /etc/oio/sds/OPENIO/oioswift-0/proxy-server.conf
+    sed -i -e "s@region = us-east-1@region = ${REGION}@" /root/.aws/config
+  else
+    REGION="us-east-1"
+  fi
+}
+
+function update_rawx(){
+  if [ ! -z "${RAWX_BETA}" ]; then
+    echo "> Activate beta rawx"
+    sed -i -e 's@/usr/sbin/httpd -D FOREGROUND  -f /etc/oio/sds/OPENIO/rawx-0/rawx-0-httpd.conf@/usr/bin/oio-rawx -s OIO,OPENIO,rawx,0 -f /etc/oio/sds/OPENIO/rawx-0/rawx-0-httpd.conf@' /etc/gridinit.d/OPENIO-rawx-0.conf
+  fi
+}
+# Initialize OpenIO cluster
+function initcluster(){
+  echo "# Initializing the OpenIO cluster"
+
+  # Temporarily disabling the swift gateway, to avoid answering when not completely setup
+  sed -i -e 's/^enabled=.*$/enabled=false/' /etc/gridinit.d/OPENIO-oioswift-0.conf
+
+	rm /etc/oio/sds/OPENIO/conscience-0/conscience-0-persistence.dat
+  echo "> Starting services"
+  /usr/bin/gridinit -d /etc/gridinit.conf >/dev/null 2>&1
+
+  echo "> Waiting for the services to start ..."
+  etime_start=$(date +"%s")
+  etime_end=$((${etime_start} + ${TIMEOUT}))
+  nbmeta1=0
+  while [ $(date +"%s") -le ${etime_end} -a ${nbmeta1} -lt ${NBREPLICAS} ]
+  do
+    sleep 2
+    # Count registered meta1
+    nbmeta1=$(/usr/bin/openio --oio-ns=${NS} cluster list meta1 -f value -c Addr|wc -l)
+  done
+  if [ ${nbmeta1} -ne ${NBREPLICAS} ]; then
+    echo "Error: Install script did not found ${NBREPLICAS} meta1 services registered. Return: ${nbmeta1}"
+    exit 1
+  fi
+  etime_start=$(date +"%s")
+  etime_end=$((${etime_start} + ${TIMEOUT}))
+  score=0
+  while [ $(date +"%s") -le ${etime_end} -a ${score} -eq 0 ]
+  do
+    /usr/bin/openio --oio-ns=${NS} cluster unlockall >/dev/null 2>&1
+    sleep 5
+    score=$(/usr/bin/openio --oio-ns=${NS} cluster list meta1 -f value -c Score || echo 0)
+  done
+  if [ ${score} -eq 0 ]; then
+    echo "Error: Unlocking scores failed. Unable to bootstrap namespace. Return: Meta1 score = ${score}"
+    exit 1
+  fi
+
+  # Initialize meta1 with 3 replicas on the same server
+  echo "> Bootstrapping directory ..."
+  /usr/bin/openio --oio-ns=${NS} directory bootstrap --replicas ${NBREPLICAS} || \
+    (echo "Error: Directory bootstrap failed. Aborting." ; exit 1)
+
+  echo "> Bootstrapping Reverse Directory and rawx for namespace ${NS}"
+  /usr/bin/openio --oio-ns=${NS} rdir bootstrap rawx ||
+    (echo "Error: Directory bootstrap failed. Aborting." ; exit 1)
+  echo "> Bootstrapping Reverse Directory and meta2 for namespace ${NS}"
+  /usr/bin/openio --oio-ns=${NS} rdir bootstrap meta2 ||
+    (echo "Error: Directory bootstrap failed. Aborting." ; exit 1)
+
+  # Stopping services
+  echo "> Stopping services ..."
+  gridinit_pid=$(cat /run/gridinit/gridinit.pid)
+  kill "${gridinit_pid}" >/dev/null 2>&1
+  /usr/bin/timeout "30s" tail --pid="${gridinit_pid}" -f /dev/null || kill -s 9 "${gridinit_pid}"
+
+}
+
+function set_unlock(){
+  sed -i -e "/^\[type:/a lock_at_first_register=false" /etc/oio/sds/*/conscience-*/conscience-*-services.conf
+}
+
+function unlock(){
+  echo "> Unlocking scores ..."
+  etime_start=$(date +"%s")
+  etime_end=$((${etime_start} + ${TIMEOUT}))
+  nbscore=1
+  while [ $(date +"%s") -le ${etime_end} -a ${nbscore} -gt 0 ]
+  do
+    /usr/bin/openio --oio-ns=${NS} cluster unlockall >/dev/null 2>&1
+    sleep 5
+    nbscore=$(/usr/bin/openio --oio-ns=${NS} cluster list -f value -c Score|grep -c -e '^0$')
+  done
+  if [ ${nbscore} -gt 0 ]; then
+    echo "Error: Unlocking scores failed."
+    exit 1
+  fi
+}
+
+function gridinit_start(){
+  echo "> Starting services ..."
+  pkill -0 -F /run/gridinit/gridinit.pid >/dev/null 2>&1 || \
+    exec /usr/bin/gridinit /etc/gridinit.conf
+}
+
+function keystone_config(){
+  if [ ! -z "$KEYSTONE_ENABLED" ]; then
+    echo "Setting up Openstack Keystone authentication"
+    : "${KEYSTONE_URI:=${IPADDR}:5000}"
+    : "${KEYSTONE_URL:=${IPADDR}:35357}"
+    : "${SWIFT_USERNAME:=swift}"
+    : "${SWIFT_PASSWORD:=SWIFT_PASS}"
+    sed -i -e "/filter:tempauth/i [filter:s3token]\ndelay_auth_decision = True\nauth_uri = http://${KEYSTONE_URL}/\nuse = egg:swift3#s3token\n\n[filter:authtoken]\nauth_type = password\nusername = ${SWIFT_USERNAME}\nproject_name = service\nregion_name = ${REGION}\nuser_domain_id = default\nmemcache_secret_key = memcache_secret_key\npaste.filter_factory = keystonemiddleware.auth_token:filter_factory\ninsecure = True\ncache = swift.cache\ndelay_auth_decision = True\ntoken_cache_time = 300\nauth_url =http://${KEYSTONE_URL}\ninclude_service_catalog = False\nwww_authenticate_uri = http://${KEYSTONE_URI}\nmemcached_servers = ${IPADDR}:6019\npassword = ${SWIFT_PASSWORD}\nrevocation_cache_time = 60\nmemcache_security_strategy = ENCRYPT\nproject_domain_id = default\n\n[filter:keystoneauth]\nuse = egg:swift#keystoneauth\noperator_roles = admin,swiftoperator,_member_\n" /etc/oio/sds/OPENIO/oioswift-0/proxy-server.conf
+    sed -i -e '/filter:tempauth/,+2d' /etc/oio/sds/OPENIO/oioswift-0/proxy-server.conf
+    sed -i -e 's@^pipeline =.*@pipeline = catch_errors gatekeeper healthcheck proxy-logging cache bulk tempurl proxy-logging authtoken swift3 s3token keystoneauth proxy-logging copy container-quotas account-quotas slo dlo versioned_writes proxy-logging proxy-server@' /etc/oio/sds/OPENIO/oioswift-0/proxy-server.conf
+  fi
+}
+
+### Main
+
+prepare
+
+# Firstboot script to setup OpenIO configuration
+if [ ! -f /etc/oio/sds/firstboot ]; then
+  echo "# Firstboot: Setting up the OpenIO cluster"
+  if [ ! -z "${OPENIO_IPADDR}" ]; then
+    IPADDR=${OPENIO_IPADDR}
+  elif [ ! -z "${OPENIO_IFDEV}" ]; then
+    IPADDR=$(ip -4 addr show ${OPENIO_IFDEV} | awk '/inet/ {print $2}' | sed 's#/.*##')
+    if [ -z "${IPADDR}" ]; then
+      echo "Error: Failed to get IP for device ${OPENIO_IFDEV}"
+    fi
+  else
+    IPADDR=$(ip -4 addr show | grep -v 127.0.0.1 | awk '/inet/ {print $2}' | sed 's#/.*##')
+  fi
+
+  # Update Swift credentials
+  update_swift_credentials
+  set_region
+  keystone_config
+
+  # Switch rawx
+  update_rawx
+
+  # Update listenning address
+  if [ ! -z "${IPADDR}" ]; then
+    echo "> Using ${IPADDR} IP address for services"
+    /usr/bin/find /etc/oio /etc/gridinit.d /root -type f -print0 | xargs --no-run-if-empty -0 sed -i "s/127.0.0.1/${IPADDR}/g"
+  fi
+
+  # Deploy OpenIO
+  initcluster
+
+  # Unlock services
+  set_unlock
+
+  # Firstboot is done
+  touch /etc/oio/sds/firstboot
+fi
+
+# Re-enabling the swift gateway, now that it's ready
+sed -i -e 's/^enabled=.*$/enabled=true/' /etc/gridinit.d/OPENIO-oioswift-0.conf
+
+# Activate logs
+/usr/sbin/rsyslogd
+
+echo "
+     .oo.   .ooo.       OIO Conscience:   ${IPADDR}:6000
+  .ooooo.   o ..oo.     OIO Proxy:        ${IPADDR}:6006
+ .oooo.     .oo. .o.
+.oooo.        .o+ .o.   S3 Endpoint:      http://${IPADDR}:6007
+iooo.          .o..oo   S3 Region:        ${REGION}
+oooo           .o, :o   S3 SSL:           False
+oooi           .o: .o   S3 Signature:     s3v4
+iooo.          .o..oo   S3 Access bucket: path-style
+.oooo.        .oo .o.   S3 Access key:    ${AKEY}
+ .oooo.     .oo. .o.    S3 Secret key:    ${SKEY}
+  .ooooo.   o...oo.
+    .ooo.   .ioo..      Visit https://docs.openio.io/latest/source/integrations for more informations.
+"
+
+# start gridinit
+gridinit_start
