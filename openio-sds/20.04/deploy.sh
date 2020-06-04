bcde91c5e88c5c3b229fd1bfc97e34837058fd00
Delgehier Cédric <cedric.delgehier@gmail.com>
2020-06-04 13:39:27 +0000

    CHANGE(DOCKER): Update README on docker hub
     ##### SUMMARY
    
    The documentation on docker hub is now pushed from the release's folder.
    
    Note: This update is done only when the release is the "latest" version.
    
     ##### IMPACT
    N/A
    
     ##### ADDITIONAL INFORMATION

--- openio-sds/20.04/deploy.sh
+++ openio-sds/20.04/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 LATEST=true
+CURRENT_DIR="$(cd "$(dirname "$0")" && pwd -P)"
 
 set -eux -o pipefail
 
@@ -19,4 +20,37 @@ if [ "${LATEST}" == "true" ]
 then
     docker tag "${DOCKER_IMAGE_NAME}" "${DEPLOY_IMAGE_NAME}:latest"
     docker push "${DEPLOY_IMAGE_NAME}:latest"
+
+    test -n "${DOCKER_HUB_USR}" || {
+        echo "Error: variable DOCKER_HUB_USR not set. Exiting."
+        exit 1
+    }
+
+    test -n "${DOCKER_HUB_PSW}" || {
+        echo "Error: variable DOCKER_HUB_PSW not set. Exiting."
+        exit 1
+    }
+
+    ## Update README
+    README_FILEPATH="${CURRENT_DIR}/DOCKERHUB_DESC.md"
+
+    echo "Acquiring token"
+    LOGIN_PAYLOAD="$(jq -n --arg username "$DOCKER_HUB_USR" --arg password "$DOCKER_HUB_PSW" '{username: $username, password: $password}')"
+    TOKEN="$(curl -s -H "Content-Type: application/json" -X POST -d "${LOGIN_PAYLOAD}" https://hub.docker.com/v2/users/login/ | jq -r .token)"
+
+    # Send a PATCH request to update the description of the repository
+    echo "Sending PATCH request"
+    REPO_URL="https://hub.docker.com/v2/repositories/openio/sds/"
+    RESPONSE_CODE="$(curl -s --write-out '%{response_code}' --output /dev/null -H "Authorization: JWT ${TOKEN}" -X PATCH --data-urlencode full_description@"${README_FILEPATH}" "${REPO_URL}")"
+    echo "Received response code: $RESPONSE_CODE"
+
+    if [ "${RESPONSE_CODE}" -eq 200 ]; then
+        exit 0
+    else
+        exit 1
+    fi
+
+
+
 fi
+
