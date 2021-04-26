9e1e8496f2b699ebf2f0769dc1abda4e11809ec2
Vincent Legoll <vincent.legoll@ovhcloud.com>
2021-04-26 09:53:09 +0200

    Remove unused images
    mirror-check: never fully ported to jenkins
    openio-debbuild: experiment never prod'ed
    openio-rpmbuild: experiment never prod'ed
    openio-repo: experiment never prod'ed

--- openio-debbuild/pbuilder.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash -x
-
-# Ensure we can run arm binaries automagically through qemu via binfmt_misc
-update-binfmts --display | grep -q 'qemu-arm (enabled)'
-if [ $? -ne 0 ]; then
-    sudo update-binfmts --enable qemu-arm
-fi
-
-pbuilder create
--- openio-repo/openio-repo-front/README.md
+++
@@ -1,3 +0,0 @@
-# To build the openio/repo-front service docker images
-
-    docker build -t openio/repo-front -f Dockerfile .
